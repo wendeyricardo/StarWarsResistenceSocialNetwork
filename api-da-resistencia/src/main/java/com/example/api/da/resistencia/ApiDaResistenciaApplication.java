e6a69d02b01b93de0449b2665da58792d2ed6afc
wendey <wendeyricardo@gmail.com>
2018-10-28 11:49:06 -0300

    commit inicial dos arquivos do projeto

---
+++ api-da-resistencia/src/main/java/com/example/api/da/resistencia/ApiDaResistenciaApplication.java
@@ -0,0 +1,12 @@
+package com.example.api.da.resistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDaResistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDaResistenciaApplication.class, args);
+	}
+}
---
+++ api-da-resistencia/src/test/java/com/example/api/da/resistencia/ApiDaResistenciaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.api.da.resistencia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiDaResistenciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
